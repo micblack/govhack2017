8c1b4977d21243e6817cf71910527fb66281d4ee
Mic Black <mic@micslab.com>
2017-07-30 08:37:47 +1000

    added notice to download the cryptophoto app

---
+++ static/components/emergency.vue
@@ -0,0 +1,10 @@
+<template>
+	<div class="emergency">If this is a medical emergency, please <el-button type="danger"><a href="tel:000">call 000</a></el-button></div>
+</template>
+
+<script>
+module.exports = {
+	name: 'personas',
+	props: []
+}
+</script>
--- static/logic/signup.js
+++ static/logic/signup.js
@@ -15,6 +15,7 @@ let vm = new Vue({
 		},
 		persona: sessionStorage.getItem('persona') || '',
 		activeTab: 'login',
+		appNotice: false,
 		serviceUrl: 'https://healthcraft.team.sh/'
 	},
 	methods: {
@@ -23,15 +24,19 @@ let vm = new Vue({
 		},
 		signUp: function () {
 			console.log('POST: ', this.serviceUrl + 'signup', this.newUser);
-			axios.post(this.serviceUrl + 'signup', this.newUser).then(() => { 
+			this.appNotice = true;
+			/*axios.post(this.serviceUrl + 'signup', this.newUser).then(() => { 
 				alert('Posted the data, should we do a location.replace or will the server return a URL?');
-			})
+			})*/
 		},
 		logIn: function () {
 			console.log('POST: ', this.serviceUrl + 'login', this.newUser);
-			axios.post(this.serviceUrl + 'login', this.newUser).then(() => { 
+			/*axios.post(this.serviceUrl + 'login', this.newUser).then(() => { 
 				alert('Posted the data, should we do a location.replace or will the server return a URL?');
-			})
+			})*/
 		},
+		confirmedAppDownload() {
+			console.log('User has confirmed they have the CryptoPhoto app');
+		}
 	}
 })
