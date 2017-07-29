56970ca40be159785cf793c8e0ed39f29e1a054e
Richard Pruss <ricpruss@gmail.com>
2017-07-29 22:34:09 +1000

    Added graphic

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-from flask import Flask
-
+from flask import Flask, render_template
+import sys
 app = Flask(__name__)
 
 # the root of the site
 @app.route("/")
 def main():
-        return "Welcome!"
+        return render_template('index.html')
 
-    if __name__ == "__main__":
-            app.run(host='0.0.0.0', port=443, ssl_context=('../healthcraft.crt', '../hea
-            lthcraft.key'))
+if __name__ == "__main__":
+     if sys.argv[1]=="--home":
+       app.run()
+     else:
+        app.run(host='0.0.0.0', port=443, ssl_context=('../healthcraft.crt', '../healthcraft.key'))
